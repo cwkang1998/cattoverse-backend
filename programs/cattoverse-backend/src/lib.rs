dc1a931fc9abc42030c27647d33b26057e48cf68
chen <23054115+cwkang1998@users.noreply.github.com>
2021-11-13 14:22:21 +0800

    chore: Change program id for devnet deploy

--- programs/cattoverse-backend/src/lib.rs
+++ programs/cattoverse-backend/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("5Sd7zzjsxKPZWT3FnYxHprUqNGzNbXhpEwKbyJ7mr9sS");
 
 #[program]
 pub mod cattoverse_backend {
