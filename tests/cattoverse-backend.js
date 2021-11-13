fb81424dc8114470e2499fbd2286a8df96558135
chen <23054115+cwkang1998@users.noreply.github.com>
2021-11-13 13:52:00 +0800

    feat: Getting basic code to run with solana

--- tests/cattoverse-backend.js
+++ tests/cattoverse-backend.js
@@ -1,14 +1,36 @@
-const anchor = require('@project-serum/anchor');
+const anchor = require("@project-serum/anchor");
 
-describe('cattoverse-backend', () => {
+// describe('cattoverse-backend', () => {
 
-  // Configure the client to use the local cluster.
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.Provider.env());
+
+//   it('Is initialized!', async () => {
+//     // Add your test here.
+//     const program = anchor.workspace.CattoverseBackend;
+//     const tx = await program.rpc.initialize();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
+
+const main = async () => {
+  console.log("🚀 Starting test...");
+
   anchor.setProvider(anchor.Provider.env());
+  const program = anchor.workspace.CattoverseBackend;
+  const tx = await program.rpc.initialize();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.CattoverseBackend;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
-});
+  console.log("Your transaction signature", tx);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+runMain();
